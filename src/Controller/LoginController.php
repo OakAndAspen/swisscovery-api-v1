73f423acad375871378008192c69e2e974d12d83
Irina Despot <irinadespot@gmail.com>
2019-01-28 14:24:33 +0100

    Implemented token autheticated access

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Service\UtilityService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api")
+ */
+
+class LoginController
+{
+    /**
+     * @Route("/signup", methods="POST")
+     *
+     * @param Request $req
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    public function signup(Request $req, EntityManagerInterface $em)
+    {
+        // Get the request elements
+        $userName = $req->get('username');
+        $email = $req->get('email');
+        $password = $req->get('password');
+        // TODO implement language checkboxes in frontend
+        //$languages = $req->get('languages');
+        $languages = ['en', 'fr'];
+
+        // Check that the userName or email is not already used
+        $user = $em->getRepository(User::class)->findBy(['userName' => $userName]);
+        if ($user) return new JsonResponse(['error' => 'User with userName ' . $userName . ' already exists']);
+        $user = $em->getRepository(User::class)->findBy(['email' => $email]);
+        if ($user) return new JsonResponse(['error' => 'User with email ' . $email . ' already exists']);
+
+        // Create the new user
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword(password_hash($password, PASSWORD_DEFAULT));
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(['success' => 'User was successfully created.']);
+    }
+
+    /**
+     * @Route("/login", methods="POST")
+     *
+     * @param Request $req
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    public function login(Request $req, EntityManagerInterface $em)
+    {
+        if (!$req->get('email') || !$req->get('password')) {
+            return new JsonResponse(['error' => 'loginMissingData']);
+        }
+
+        $user = $em->getRepository(User::class)->findOneBy([
+            'email' => $req->get('email')
+        ]);
+
+        if (!$user) return new JsonResponse(['error' => 'User not found']);
+
+        if (!password_verify($req->get('password'), $user->getPassword())) {
+            return new JsonResponse(['error' => 'loginPwIncorrect']);
+        }
+
+        // Create a JWT
+        $jwt = UtilityService::generateJWT($user);
+        $em->flush();
+
+        return new JsonResponse(['authKey' => $jwt]);
+    }
+}
