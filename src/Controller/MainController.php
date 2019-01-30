4132e2e3f007f74b48c7c764b18d90f51a0f12a7
Irina Despot <irinadespot@gmail.com>
2019-01-30 14:34:18 +0100

    Serve features

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\PointOfInterest;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController implements TokenAuthenticatedController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/features/{canton}", name="main")
+     *
+     * @param EntityManagerInterface $em
+     * @param $canton
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function index()
+    public function serveFeatures(EntityManagerInterface $em, $canton)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
+        $features = $em->getRepository(PointOfInterest::class)->findBy([
+            "canton" => strtoupper($canton)
         ]);
+
+        $fColl = [
+            "type" => "FeatureCollection",
+            "features" => []
+        ];
+
+        foreach ($features as $f) {
+            array_push($fColl['features'], [
+                "type" => "Feature",
+                "id" => $f->getId(),
+                "geometry" => [
+                    "type" => "Point",
+                    "coordinates" => [$f->getX(), $f->getY()]
+                ],
+                "properties" => [
+                    "id" => $f->getId(),
+                    "featureId" => $f->getFeatureId(),
+                    "label" => $f->getLabel(),
+                    "canton" => $f->getCanton(),
+                    "commune" => $f->getCommune(),
+                    "wikiTitle" => $f->getWikiTitle(),
+                    "wikiLink" => $f->getWikiLink(),
+                    "category" => $f->getCategory()
+                ]
+            ]);
+        }
+
+        return $this->json($fColl);
     }
 }
