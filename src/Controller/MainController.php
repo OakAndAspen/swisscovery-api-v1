4823725a8a4ae3054a6a52962950da2301c7aee7
Irina Despot <irinadespot@gmail.com>
2019-01-30 17:04:38 +0100

    POST comment

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,25 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\PointOfInterest;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController implements TokenAuthenticatedController
 {
     /**
-     * @Route("/features/{canton}", name="main")
+     * @Route("/features/{canton}", name="serve-features", methods={"GET"})
      *
      * @param EntityManagerInterface $em
      * @param $canton
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function serveFeatures(EntityManagerInterface $em, $canton)
+    public function serveFeatures(Request $req, EntityManagerInterface $em, $canton)
     {
-        $features = $em->getRepository(PointOfInterest::class)->findBy([
+        $user = $req->get("user");
+        $pois = $em->getRepository(PointOfInterest::class)->findBy([
             "canton" => strtoupper($canton)
         ]);
 
@@ -29,27 +32,87 @@ class MainController extends AbstractController implements TokenAuthenticatedCon
             "features" => []
         ];
 
-        foreach ($features as $f) {
-            array_push($fColl['features'], [
+        foreach ($pois as $poi) {
+            $comment = $em->getRepository(Comment::class)->findOneBy([
+                "user" => $user,
+                "pointOfInterest" => $poi
+            ]);
+
+            $feature = [
                 "type" => "Feature",
-                "id" => $f->getId(),
+                "id" => $poi->getId(),
                 "geometry" => [
                     "type" => "Point",
-                    "coordinates" => [$f->getX(), $f->getY()]
+                    "coordinates" => [$poi->getX(), $poi->getY()]
                 ],
                 "properties" => [
-                    "id" => $f->getId(),
-                    "featureId" => $f->getFeatureId(),
-                    "label" => $f->getLabel(),
-                    "canton" => $f->getCanton(),
-                    "commune" => $f->getCommune(),
-                    "wikiTitle" => $f->getWikiTitle(),
-                    "wikiLink" => $f->getWikiLink(),
-                    "category" => $f->getCategory()
+                    "id" => $poi->getId(),
+                    "featureId" => $poi->getFeatureId(),
+                    "label" => $poi->getLabel(),
+                    "canton" => $poi->getCanton(),
+                    "commune" => $poi->getCommune(),
+                    "wikiTitle" => $poi->getWikiTitle(),
+                    "wikiLink" => $poi->getWikiLink(),
+                    "category" => $poi->getCategory()
                 ]
-            ]);
+            ];
+
+            if ($comment) {
+                $feature['properties']['comment'] = [
+                    "isInterested" => $comment->getIsInterested(),
+                    "hasVisited" => $comment->getHasVisited(),
+                    "text" => $comment->getText(),
+                    "images" => $comment->getImages()
+                ];
+            } else {
+                $feature['properties']['comment'] = [
+                    "isInterested" => false,
+                    "hasVisited" => false,
+                    "text" => "",
+                    "images" => []
+                ];
+            }
+
+            array_push($fColl['features'], $feature);
         }
 
         return $this->json($fColl);
+    }
+
+    /**
+     * @Route("/comment/{poiId}", name="comment", methods={"POST"})
+     * @param Request $req
+     * @param EntityManagerInterface $em
+     * @param $poiId
+     * @return JsonResponse
+     */
+    public function comment(Request $req, EntityManagerInterface $em, $poiId)
+    {
+        $user = $req->get("user");
+        $poi = $em->getRepository(PointOfInterest::class)->find($poiId);
+        $comment = $em->getRepository(Comment::class)->findOneBy([
+            "user" => $user,
+            "pointOfInterest" => $poi
+        ]);
+
+        if (!$comment) {
+            $comment = new Comment();
+            $comment->setUser($user);
+            $comment->setPointOfInterest($poi);
+            $comment->setIsInterested(false);
+            $comment->setHasVisited(false);
+            $comment->setText("");
+            $comment->setImages([]);
+        }
+
+
+        if ($req->get("isInterested")) $comment->setIsInterested($req->get("isInterested"));
+        if ($req->get("hasVisited")) $comment->setHasVisited($req->get("hasVisited"));
+        if ($req->get("text")) $comment->setText($req->get("text"));
+
+        $em->persist($comment);
+        $em->flush();
+
+        return new JsonResponse(["success" => "Comment was updated"]);
     }
 }
